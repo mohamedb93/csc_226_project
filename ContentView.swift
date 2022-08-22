f0a7d6d957de04dd1ca61508ff9c0e7c84fd0100
mohamedb93 <96845775+mohamedb93@users.noreply.github.com>
2022-08-21 22:59:40 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Color.blue.edgesIgnoringSafeArea(.all)
+        VStack {
+            Text("Hello, world!")
+            Text("First App")
+        }
+    }
+}
+
